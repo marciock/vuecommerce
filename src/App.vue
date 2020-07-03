29b9ca288a65525ca81381561394fffeb84bdd84
marciock <marciockalves@gmail.com>
2020-07-03 19:27:01 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <NavBar></NavBar>
+    <div class="container">
+      <Home></Home>
+
+      <div class="row">
+        <Icons v-for="m in menu" v-bind:key="m" :icon="m.icon" :title="m.title"></Icons>
+      </div>
+
+     
+
+    
+    </div>
+
+  </div>
+</template>
+
+<script>
+ import 'bootstrap/dist/css/bootstrap.css';
+ import 'font-awesome/css/font-awesome.css';
+ import NavBar from './components/layouts/NavBar';
+ import Home from './components/layouts/Home';
+ import Icons from './components/layouts/Icons';
+ 
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+    Home,
+    Icons,
+    
+    
+  },
+  data:()=>{
+        return{
+            menu:[
+                    {'title':'Produtos','icon':require('@/assets/icons/produtos.svg')},
+                    {'title':'Marcas','icon':require('@/assets/icons/marca.svg')},
+                    {'title':'Imagens','icon':require('@/assets/icons/imagens.svg')},
+                
+                ] 
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/layouts/Icons.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <div class="col" >
+        <div class="text-center">
+            <a href="">
+                <img :src="icon" style="width: 128px;height:auto;" >
+                <h5 style="margin-top: 0;">{{title}}</h5>
+            </a>
+        </div>
+    </div>
+    
+</template>
+<script>
+export default {
+    name:'Icons',
+    props:['icon','title']
+    
+}
+</script>
